d3bf9533b9398497b70fb7d1b4dca1c0b08657f8
ankityadav.reh@gmail.com <167362173+Srvankit@users.noreply.github.com>
2026-07-24 10:48:43 +0530

    Initial Spring Boot backend setup

---
+++ Velora-Backend/src/test/java/com/veloramarkets/VeloraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veloramarkets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
